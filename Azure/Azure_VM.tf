43dc2903f094ed1b7fc689b4230774b53ede8cb9
ravicwp <109152895+ravicwp@users.noreply.github.com>
2022-07-13 19:17:46 +0530

    Update Azure_VM.tf

--- Azure/Azure_VM.tf
+++ Azure/Azure_VM.tf
@@ -39,7 +39,7 @@ resource "azurerm_key_vault" "example" {
 }
 
 resource "azurerm_key_vault" "example" {
-  name                        = "des-example-keyvault_ravi"
+  name                        = "des-example-keyvault_ravi1"
   location                    = azurerm_resource_group.example.location
   resource_group_name         = azurerm_resource_group.example.name
   tenant_id                   = data.azurerm_client_config.current.tenant_id
