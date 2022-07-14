7497265f8da1022367715cb214875098a90863ec
ravicwp <109152895+ravicwp@users.noreply.github.com>
2022-07-14 12:18:12 +0530

    Update gcp_bucketlevel.tf

--- GCP/gcp_bucketlevel.tf
+++ GCP/gcp_bucketlevel.tf
@@ -1,7 +1,7 @@
 provider "google"{
  region="us.west"
 }
-resource "google_storage_bucket" "Bucketlevel1" {
+resource "google_storage_bucket" "Bucketlevel2" {
     name     = "TestBucket1"
     bucket_policy_only = true
     uniform_bucket_level_access = false
