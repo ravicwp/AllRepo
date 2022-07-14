52b1472e2b9716ff2673df04f697d0b79922b945
ravicwp <109152895+ravicwp@users.noreply.github.com>
2022-07-14 12:21:17 +0530

    Update Azure_VM.tf

--- Azure/Azure_VM.tf
+++ Azure/Azure_VM.tf
@@ -1,4 +1,4 @@
-resource "azurerm_managed_disk" "example1" {
+resource "azurerm_managed_disk" "example2" {
   name                 = var.disk_name
   location             = var.location
   resource_group_name  = var.resource_group_name
