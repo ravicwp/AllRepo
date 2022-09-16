95ab42022378ad2600ff234b9faee961f6f65f83
ravicwp <109152895+ravicwp@users.noreply.github.com>
2022-09-16 18:08:51 +0530

    Update KDaemonSet.tf

--- Kubernetes/KDaemonSet.tf
+++ Kubernetes/KDaemonSet.tf
@@ -18,3 +18,24 @@ resource "kubernetes_daemonset" "ravi123" {
     }
   }
 }
+
+resource "kubernetes_daemonset" "ravi321" {
+  metadata {
+    name      = "terraform-example"
+    namespace = "something"
+    labels = {
+      test = "MyExampleApp"
+    }
+    tag = {
+      tester = "ravi"
+    }
+  }
+  
+  spec {
+    selector {
+      match_labels = {
+        test = "ravi321"
+      }
+    }
+  }
+}
