abcb41d8adf892ffafee6d4df596d85a3d219ffd
ravicwp <109152895+ravicwp@users.noreply.github.com>
2022-09-27 16:30:07 +0530

    Update gcp_bucketlevel.tf

--- GCP/gcp_bucketlevel.tf
+++ GCP/gcp_bucketlevel.tf
@@ -11,3 +11,24 @@ resource "google_storage_bucket" "ravitest1" {
     bucket_policy_only = true
     uniform_bucket_level_access = false
 }
+
+esource "google_storage_bucket" "ravitest21" {
+    name     = "TestBucket1"
+    bucket_policy_only = true
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "ravitest12" {
+    name     = "TestBucket1"
+    bucket_policy_only = true
+    uniform_bucket_level_access = false
+}
+esource "google_storage_bucket" "ravitest23" {
+    name     = "TestBucket1"
+    bucket_policy_only = true
+    uniform_bucket_level_access = false
+}
+resource "google_storage_bucket" "ravitest13" {
+    name     = "TestBucket1"
+    bucket_policy_only = true
+    uniform_bucket_level_access = false
+}
